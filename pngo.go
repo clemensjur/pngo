f32099e49d355b0f25cf19391de132ad97cfcfd2
Clemens Jurkowitsch <clemens.jurkowitsch@gmx.at>
2023-08-29 21:58:29 +0200

    Read chunk type, data and crc values

--- pngo.go
+++ pngo.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/binary"
+	"fmt"
 	"os"
 )
 
@@ -11,6 +12,13 @@ func check(e error) {
 	}
 }
 
+type PngChunk struct {
+	length uint32
+	ctype  uint32
+	data   []byte
+	crc    uint32
+}
+
 func main() {
 	dat, err := os.ReadFile("img.png")
 	check(err)
@@ -18,4 +26,23 @@ func main() {
 	if binary.BigEndian.Uint64(dat[0:8])&uint64(0x89504e470d0a1a0a) != 0x89504e470d0a1a0a {
 		panic("PNG header not valid!")
 	}
+
+	var cstart uint32 = 8
+	for {
+		var length uint32 = binary.BigEndian.Uint32(dat[cstart : cstart+4])
+		var ctype string = string(dat[cstart+4 : cstart+8])
+		var data []byte = dat[cstart+8 : cstart+8+length]
+		var crc []byte = dat[cstart+8+length : cstart+12+length]
+
+		fmt.Printf("TYPE: %s\n", ctype)
+		fmt.Printf("DATA: %x\n", data)
+		fmt.Printf("CRC: %x\n\n", crc)
+
+		if ctype == "IEND" {
+			break
+		}
+
+		cstart += 12 + length
+	}
+
 }
